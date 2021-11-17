66e8f67b8ba5159dee35707871428bceb62ffbdd
Martha Staus <mjstaus@gmail.com>
2021-11-17 08:41:57 -0800

    initial commit, pair programming with Kyril

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,151 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically and has less characters than the length of the column or row", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ELLIE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally and has less characters than the length of the column or row", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Z', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'L', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ELLIE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally AND vertically and has less characters than the length of the column or row", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'L', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ELLIE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is lower case", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'Z', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ellie')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word contains upper and lower case characters", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'Z', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ElLie')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if an empty string is passed in", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'Z', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if undefined is passed in", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'Z', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], undefined)
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if a number is passed in", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'Z', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 94847)
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if a word in present in a non-square matrix", function() {
+    const result = wordSearch([
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Z', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Z', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'Z', 'L', 'I', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'E', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'L', 'A', 'L'],
+      ['O', 'D', 'C', 'A', 'K', 'L', 'A', 'S'],
+      ['O', 'D', 'C', 'A', 'K', 'I', 'A', 'S'],
+      ['O', 'D', 'C', 'A', 'K', 'E', 'A', 'S'],
+    ], 'ELLIE')
+
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,54 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (!word || typeof word !== 'string'){
+        return false
     }
+    word = word.toUpperCase();
+
+    let words = [];
+
+    //horizontal looping
+    for (let item in letters) {
+        let word = letters[item].join('');
+        words.push(word);
+    }
+
+    //vertical looping
+    for (let x = 0; x < letters[0].length; x++) {
+        let array = [];
+        for (let y = 0; y <letters.length; y++) {
+            array.push(letters[y][x]) 
+        }let word = array.join('')
+        words.push(word);
+    }
+    //console.log(words)
+
+    //check if string is one of the items in the arrays
+    for (let item of words){
+        if(item.includes(word)){
+            console.log(word)
+            return true;
+        }
+    }
+    return false;
 }
 
+console.log(wordSearch([
+    ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'R', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'A', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'K', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], undefined));
+
+//
+
 module.exports = wordSearch
+
+
+
+
+
